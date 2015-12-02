79865a9f5721d793f4ab89411d783241df05ccb7
edmilliano <eeke24@gmail.com>
2015-12-01 21:28:36 -0800

    Completed ex_addressbook and ex_fizz_buzz and worked on ex_teddit_hashes

--- 04_Loops_Classes/starter_code/ex_addressbook.rb
+++ 04_Loops_Classes/starter_code/ex_addressbook.rb
@@ -9,42 +9,59 @@
 require 'pry'
 
 def contacts_count(contacts)
+	contacts.length
 end
 
 #create a contact using the hash syntax
 def create_contact(name, telephone, email)
+	{name: name, telephone: telephone, email: email}
 end
 
 #iterate through all contacts. Print THE name, email & telephone using iterpolation "#{this_syntax_is} interpolation" 
 def contact_log(contacts)
+	contacts.each do |contact|
+		puts "#{contact[:name]}, #{contact[:email]}, #{contact[:telephone]}"
+	end 
 end
 
 #find a specific contact and print the telephone number
 def find_and_call_contact(contacts, name)
+	found_name = {}
+	contacts.each do |contact|
+		puts "#{contact[:name]} == #{name}"
+		if contact[:name] == name
+			found_name = contact
+		end
+	end 
+	puts "Look what we found #{found_name}"
+	found_name
 end
 
 def add_to_contacts(contacts, contact)
+	contacts << contact 
 end
 
 #array to hold each contact
 contacts = []
 
 # #Examples. Only directional you can do this whatever way you like
-# contact = create_contact("kisha", "555-555-5555", "kisha@example.com")
-# add_to_contacts(contacts, contact)
+contact = create_contact("kisha", "555-555-5555", "kisha@example.com")
+add_to_contacts(contacts, contact)
 #
-# contact = create_contact("david", "222-222-2222", "david@example.com")
-# add_to_contacts(contacts, contact)
+contact = create_contact("david", "222-222-2222", "david@example.com")
+add_to_contacts(contacts, contact)
 #
-# contact = create_contact("nicole", "444-444-4444", "nicole@example.com")
-# add_to_contacts(contacts, contact)
+contact = create_contact("nicole", "444-444-4444", "nicole@example.com")
+add_to_contacts(contacts, contact)
+
+#puts "#{contacts}"
 #
 # ####
-# contacts_count(contacts)
-#
-# #find_a_name
-# name = "kisha"
-# find_and_call_contact(contacts, name)
+puts "#{contacts_count(contacts)}"
+
+# find_a_name
+name = "kisha"
+ find_and_call_contact(contacts, name)
 #
 # #contact_logs
-# contact_log(contacts)
+contact_log(contacts)
