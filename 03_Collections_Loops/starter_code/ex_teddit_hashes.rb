509efa13c50b680d6e7048130eb6f5363810b95e
edmilliano <eeke24@gmail.com>
2015-12-03 17:32:59 -0800

    Completed step 2 and updated the logic for calculating upvotes

--- 03_Collections_Loops/starter_code/ex_teddit_hashes.rb
+++ 03_Collections_Loops/starter_code/ex_teddit_hashes.rb
@@ -8,6 +8,14 @@
 # Add the hash to an array called stories and print "Story: Monkeys thank mayor for flounder tooth necklace, Category: (Teeth), Current Upvotes: 1"
 # Using the stories array 
 # Test your cat, bacon, and food upvote conditional logic.
+
+# If the Story has the word 'cat' multiply the upvotes by 5
+    # If the Story has the word 'bacon' multiply the upvotes by 8
+    # NOTE: look at the ruby docs to see how to check if a string has another string in it!
+    # (google something like "ruby string contains substring")
+    # NOTE: capitalization isn't important so consider using the "downcase" method (google it!)
+    # If the Story's category is 'food' it gets 3 times the upvotes.
+
 #
 # Step 2: Adding Loops 
 # Use a loop, so that your program continues to ask a user for stories until they chose to quit. ("Would you like to add another story? Enter 'y' or 'n'")
@@ -25,10 +33,10 @@ def calculate_upvotes(story)
   # Could I also put here story = [title:title, category:category]? 
   if title.downcase.include? 'cat'
     upvotes *= 5
-  elsif title.downcase.include? 'bacon'
+  end
+  if title.downcase.include? 'bacon'
     upvotes *= 8
   end
-
   if category.downcase == "food"
     upvotes *= 3
   end
@@ -36,25 +44,7 @@ def calculate_upvotes(story)
   upvotes
 end
 
-stories = []
-
-puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"
-story = {}
-puts "Please enter a News story:"
-title = get_input
-story[:title] = title
-# could I also do story = [title: title]?
-# could also do: 
-# story[:title] = get_input
-puts "Please give it a category:"
-story[:category] = get_input
-story[:upvotes] = calculate_upvotes(story)
-stories << story
-
-puts "Would you like to add another story? Enter 'y' or 'n'"
-answer = get_input.downcase.to_s
-
-while answer == "y" do
+def collectstory
   puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"
   story = {}
   puts "Please enter a News story:"
@@ -66,13 +56,18 @@ while answer == "y" do
   puts "Please give it a category:"
   story[:category] = get_input
   story[:upvotes] = calculate_upvotes(story)
-
-  puts "Would you like to add another story? Enter 'y' or 'n'"
-  answer = get_input.downcase.to_s
-  stories << story
+  story
 end
 
-puts stories
+stories = []
+
+answer = "y" 
+
+while answer == "y" do
+  stories << collectstory()
+  puts "Would you like to add another story? Enter 'y' or 'n'"
+  answer = get_input.downcase.to_s
+end
 
 stories.each do |story|
   puts "New story added! #{story[:title]}, Category: #{story[:category].capitalize}, Current Upvotes: #{story[:upvotes]}"
