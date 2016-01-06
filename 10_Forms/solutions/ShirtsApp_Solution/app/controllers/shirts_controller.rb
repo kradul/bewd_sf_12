3d8ce75a2620d76e9baee11deeb9010be0a0366f
Katya Radul <katya.radul@gmail.com>
2016-01-05 20:33:41 -0800

    solution for shirt app

---
+++ 10_Forms/solutions/ShirtsApp_Solution/app/controllers/shirts_controller.rb
@@ -0,0 +1,46 @@
+require 'pry'
+
+class ShirtsController < ApplicationController
+  before_action :load_shirt, only: [:edit, :show, :update]
+
+  def index
+    @shirts = Shirt.search_for(params[:q])
+  end
+
+  def new
+    @shirt = Shirt.new
+  end
+
+  def create
+    @shirt = Shirt.new(safe_shirt_params)
+    if @shirt.save
+      redirect_to @shirt
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @shirt.update shirt_params
+    redirect_to @shirt
+  end
+
+  def show
+  end
+
+  private
+
+    def safe_shirt_params
+      params.require('shirt').permit(:name, :description, :image)
+    end
+
+    def load_shirt
+      @shirt = Shirt.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash.now[:notice] = "Invalid Shirt ID #{params[:id]}"
+      redirect_to root_path
+    end
+end
